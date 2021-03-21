e0b22f2ba36bf7c729c5c82070cd6ce9783c92ea
janu01ary <janu01ary@gmail.com>
2021-03-21 16:47:02 +0900

    debug true

--- announcement/settings.py
+++ announcement/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'smoak37s6b@!w%km+@u-bp1$t4#bp%py4mfe3v_*zhh5fjvlbp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 

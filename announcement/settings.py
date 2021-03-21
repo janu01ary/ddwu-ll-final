f8fa6a613676738e279a875f9569668e0f8a37f3
janu01ary <janu01ary@gmail.com>
2021-03-21 16:57:51 +0900

    add heroku setting

--- announcement/settings.py
+++ announcement/settings.py
@@ -131,3 +131,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR / 'static/')
 SASS_PROCESSOR_ROOT = STATIC_ROOT
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)

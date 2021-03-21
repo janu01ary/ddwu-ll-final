31e9ad4aeab78c18137bc71b249bff8cf77c52f4
janu01ary <janu01ary@gmail.com>
2021-03-21 15:40:13 +0900

    heroku ready

--- announcement/settings.py
+++ announcement/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'smoak37s6b@!w%km+@u-bp1$t4#bp%py4mfe3v_*zhh5fjvlbp'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'smoak37s6b@!w%km+@u-bp1$t4#bp%py4mfe3v_*zhh5fjvlbp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -52,7 +52,7 @@ STATICFILES_FINDERS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
